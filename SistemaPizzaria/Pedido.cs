836024073bf83552a609e961c7f2cb3d94696188
Ana <anacarolinathbg@gmail.com>
2025-08-27 17:41:35 -0300

    Adicionando os elementos no form pedido

--- SistemaPizzaria/Pedido.cs
+++ SistemaPizzaria/Pedido.cs
@@ -16,5 +16,10 @@ namespace SistemaPizzaria
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
